219b5b7b70ee1e63bbeee0d26c26c846c7c87aec
Unknown <akshaybharambe14@gmail.com>
2018-12-08 18:21:42 +0530

    Add setup files
    1. Add setup files

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'none',
+  tabWidth: 2,
+  bracketSpacing: true,
+  printWidth: 100,
+  jsxBracketSameLine: false,
+  htmlWhitespaceSensitivity: 'css'
+}
